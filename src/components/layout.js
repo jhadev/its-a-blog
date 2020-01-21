950fb33bdcaa3aac8aa70d004fbeab458562739e
jhadev <joshappeldev@gmail.com>
2020-01-21 15:10:40 -0500

    global css

---
+++ src/components/layout.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Global, css } from '@emotion/core';
+
+// lobotomized owl selector * + * = every sibling selection is going to get this style
+// avoid css reset normalizes vertical spacing
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
+            margin: 0;
+          }
+
+          * + * {
+            margin-top: 1rem;
+          }
+
+          html,
+          body {
+            margin: 0;
+            color: #555;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Helvetica, Arial, sans-serif;
+            font-size: 18px;
+            line-height: 1.4;
+
+            /* remove margin for main div that gatbsy mounts into */
+
+            > div {
+              margin-top: 0;
+            }
+
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6 {
+              color: #222;
+              line-height: 1.1;
+
+              + * {
+                margin-top: 0.5rem;
+              }
+            }
+
+            strong {
+              color: #222;
+            }
+
+            li {
+              margin-top: 0.25rem;
+            }
+          }
+        `}
+      />
+      <header></header>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => {
   return (
-    <>
+    <Layout>
       <h1>Yo Yo Yo Yo!</h1>
       <p>I'm boring so there is nothing here now.</p>
       <Link to="/">take me home</Link>
-    </>
+    </Layout>
   );
 };
