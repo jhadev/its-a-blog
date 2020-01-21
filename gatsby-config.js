c740199e9bb980743075f16fe7046f61a9c9196a
jhadev <joshappeldev@gmail.com>
2020-01-21 18:03:23 -0500

    setup gatsby mdx plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsy-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js')
+        }
+      }
+    }
+  ],
   siteMetadata: {
     title: "it's my blog, yo!",
     description: 'just some projects and stuff.'
