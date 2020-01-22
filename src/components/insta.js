bfeb35e66c9a7ad7bc4ec40e03b35fb6d6b44e07
jhadev <joshappeldev@gmail.com>
2020-01-22 11:34:05 -0500

    scaffold insta component

---
+++ src/components/insta.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'gatsby-image';
+import { css } from '@emotion/core';
+
+const Insta = () => {
+  return (
+    <>
+      <h2>insta posts from @meme_coding</h2>
+      <div>{/* posts go here */}</div>
+      <a
+        href="http://instagram.com/meme_coding"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        see more on ig
+      </a>
+    </>
+  );
+};
+
+export default Insta;
